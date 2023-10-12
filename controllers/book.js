33cee2037055b13a054e8edac4e5a882e9ce4879
Vbrandel <v.brandel76@gmail.com>
2023-10-12 23:00:10 +0200

    commentaire pour la soutenance

--- controllers/book.js
+++ controllers/book.js
@@ -1,17 +1,22 @@
 const Book = require('../models/book');
 const fs = require('fs');
 
-
+// Création d'un livre
  exports.createBook = (req, res, next) => {
     console.log(req.file.filename);
+    // Chaine JSON -> Objet JS
      const bookObject = JSON.parse(req.body.book);
+    // Delete ID générer
      delete bookObject._id;
      delete bookObject._userId;
+     // Création du livre
      const book = new Book({
          ...bookObject,
+         // Utilisation du token et création du path de l'image
          userId: req.auth.userId,
          imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
      });
+    // Sauvegarde
      book.save()
      .then(() => {
         res.status(201).json({ message: 'Livre enregistré !'});
@@ -19,6 +24,7 @@ const fs = require('fs');
      .catch(error => res.status(400).json({ error }));
 };
 
+// Récuperer les livres
 exports.getAllBooks = (req, res, next) => {
     Book.find().then(
         (books) => {
@@ -33,6 +39,7 @@ exports.getAllBooks = (req, res, next) => {
     );
 };
 
+// Récupère l'ID d'un livre
 exports.getOneBook = (req, res, next) => {
     Book.findOne({ _id: req.params.id })
     .then((book) => {
@@ -45,12 +52,16 @@ exports.getOneBook = (req, res, next) => {
     .catch((error) => res.status(500).json({ error }));
 };
 
-
+// Calcul de la note moyenne
 exports.averageRating = (req, res, next) => {
+    // Récupération du livre associé à l'ID
     Book.findOne({ _id: req.params.id })
     .then((book) => {
+        //Mise à jour de la note du livre
         book.ratings.push({ userId: req.auth.userId, grade: req.body.rating });
         book.averageRating =
+
+        // Accumulation des notes et note sur 5
         book.ratings.reduce((acc, rating) => acc + rating.grade, 0) /
         book.ratings.length;
         
@@ -62,6 +73,7 @@ exports.averageRating = (req, res, next) => {
         .catch((error) => res.status(404).json({ error: 'Le livre est introuvable' }));
     };
 
+// Les meilleurs livres
 exports.bestratingBook = (req, res, next) => {
     Book.find()
         .sort({ averageRating: -1 })
@@ -70,61 +82,31 @@ exports.bestratingBook = (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
      };
 
-// exports.modifyBook = (req, res, next) => {
-//     const bookObject = req.file
-//           ? {
-//               ...JSON.parse(req.body.book),
-//               imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-//             }
-//           : { ...req.body };
-//         delete bookObject.userId;
-      
-//         Book.findOne({ _id: req.params.id })
-//           .then((book) => {
-//             if (book.userId !== req.auth.userId) {
-//               res.status(401).json({ message: "Vous n'êtes pas le créateur de ce livre." });
-//             } else {
-//               if (bookObject.imageUrl) {
-//                 const filename = book.imageUrl.split('/images/')[1];
-//                 fs.unlink(`images/${filename}`, () => { 
-//                     updateNewBook();
-//                 });
-//               } else {
-//                 updateNewBook();
-//               }
-//             }
-//           })
-//           .catch((error) => {
-//             res.status(400).json({ error });
-//           });
-      
-//         function updateNewBook() {
-//           Book.updateOne(
-//             { _id: req.params.id },
-//             { ...bookObject, _id: req.params.id }
-//           )
-//             .then(() => {
-//               res.status(200).json(bookObject);
-//             })
-//             .catch((err) => res.status(401).json({ error }));
-//         }
-// };
 
-// Controller pour modifier un livre
+// Modifier un livre
 exports.modifyBook = (req, res, next) => {
+  // Récupération de l'image si on la change
   const bookObject = req.file ? {
     ...JSON.parse(req.body.book),
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename.split('.')[0]}.webp`
-  } : { ...req.body };
+  }
+  // Sinon récupere dans le corps de la requête
+   : { ...req.body };
+   // Suppression de l'ID
   delete bookObject._userId;
+  // Récupération du livre associé à l'ID
   Book.findOne({_id: req.params.id})
     .then((book) => {
+      // Vérification si le livre appartient bien à l'utilisateeur connecté
       if (book.userId != req.auth.userId) {
         res.status(401).json({ message: 'Pas d autorisation' });
       } else if (req.file) {
+        // Récupération de l'image
         const filename = book.imageUrl.split('/images')[1];
+        // Suppression de l'image
         fs.unlink(`images/${filename}`, () => { });     
       }
+      // Sauvegarde
       Book.updateOne({_id: req.params.id}, {...bookObject, _id: req.params.id})
         .then(() => res.status(200).json({ message: 'Livre modifié' }))
         .catch(error => res.status(401).json({ error }));
@@ -132,22 +114,24 @@ exports.modifyBook = (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 };
 
-
-
-
+// Suppression d'un livre
 exports.deleteBook = (req, res, next) => {
+  // Récupération de l'ID du livre
     Book.findOne({ _id: req.params.id })
       .then((book) => {
+      // Vérification si le livre appartient bien à l'utilisateeur connecté
         if (!book) {
           res.status(404).json({ message: 'Livre introuvable' });
         } else if (book.userId !== req.auth.userId) {
           res.status(401).json({ message: 'Pas autorisé' });
         } else {
+          // Récupération de l'image
           const filename = book.imageUrl.split('/images')[1];
+          // Suppression de l'image
           fs.unlink(`images/${filename}`, () => {
             Book.deleteOne({ _id: req.params.id })
               .then(() => {
-                res.status(200).json({ message: 'Livre supprimé !' });
+                res.status(204).send();
               })
               .catch((error) => res.status(401).json({ error }));
           });
--- controllers/user.js
+++ controllers/user.js
@@ -2,13 +2,16 @@ const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
+// Création utilisateur
 exports.signup = (req, res, next) => {
+  // Récupération du mail et hachage du mdp via bcrypt
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
         const user = new User({
             email: req.body.email,
             password: hash
         });
+        // Sauvegarde de l'utilisateur
         user.save()
         .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
         .catch(error => res.status(400).json({ error }));
@@ -21,14 +24,17 @@ exports.login = (req, res, next) => {
   
     User.findOne({ email: email })
       .then(user => {
+        // Si le mail et le mot de passe sont strictement nul alors erreur
         if (user === null) {
           res.status(401).json({ message: 'Pair identifiant invalide' });
         } else {
           bcrypt.compare(password, user.password)
             .then(valid => {
+              // Si le hash est différent de la reequete alors erreur
               if (!valid) {
                 res.status(401).json({ message: 'Pair identifiant invalide' });
               } else {
+                // Si valide -> connexion pendant 24h
                 res.status(200).json({
                   userId: user._id,
                   token: jwt.sign({ userId: user._id }, 'RANDOM_TOKEN_SECRET',{ expiresIn: '24h' })
